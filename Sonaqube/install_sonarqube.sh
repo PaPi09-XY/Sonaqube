f5f8d4715ef9297c6558ce89159d7305a991f453
PaPi09-XY <kingighe@gmail.com>
2024-09-11 11:58:01 +0100

    Update install_sonarqube.sh

--- Sonaqube/install_sonarqube.sh
+++ Sonaqube/install_sonarqube.sh
@@ -1,58 +1,79 @@
 #!/bin/bash
+sudo cp /etc/sysctl.conf /root/sysctl.conf_backup
 
-# Update the package manager
-sudo apt-get update -y
-
-# Install required dependencies
-sudo apt-get install -y openjdk-11-jdk wget unzip ufw
-
-# Download and install SonarQube
-cd /opt
-sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.6.1.59531.zip
-sudo unzip sonarqube-9.6.1.59531.zip
-sudo mv sonarqube-9.6.1.59531 sonarqube
-sudo useradd -m -d /opt/sonarqube sonarqube
-
-# Set permissions
-sudo chown -R sonarqube:sonarqube /opt/sonarqube
-
-# Configure SonarQube memory settings and host binding
-sudo bash -c 'cat <<EOF >> /opt/sonarqube/conf/sonar.properties
-sonar.web.javaOpts=-Xmx512m -Xms128m -XX:+HeapDumpOnOutOfMemoryError
-sonar.search.javaOpts=-Xms512m -Xmx512m -XX:+HeapDumpOnOutOfMemoryError
-sonar.log.level=INFO
-sonar.path.logs=logs
-sonar.web.host=0.0.0.0
+# Modify Kernel System Limits for Sonarqube
+    sudo sh -c 'cat <<EOF> /etc/sysctl.conf
+    vm.max_map_count=262144
+    fs.file-max=65536
+    ulimit -n 65536
+    ulimit -u 4096
 EOF'
+    sudo apt update -y
+    sudo apt-get install openjdk-11-jdk -y
 
-# Start SonarQube
-sudo -u sonarqube /opt/sonarqube/bin/linux-x86-64/sonar.sh start
+# Postgres Database installation and setup
+    wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
 
-# Enable SonarQube to start on boot
-sudo bash -c 'cat <<EOF > /etc/systemd/system/sonarqube.service
-[Unit]
-Description=SonarQube service
-After=syslog.target network.target
+    sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+    sudo apt install postgresql postgresql-contrib -y
 
-[Service]
-Type=forking
-ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
-ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
-User=sonarqube
-Group=sonarqube
-Restart=always
+    sudo systemctl enable postgresql.service
+    sudo systemctl start  postgresql.service
+    sudo echo "postgres:admin123" | sudo chpasswd
+    sudo runuser -l postgres -c "createuser sonar"
+    sudo -i -u postgres psql -c "ALTER USER sonar WITH ENCRYPTED PASSWORD 'admin123';"
+    sudo -i -u postgres psql -c "CREATE DATABASE sonarqube OWNER sonar;"
+    sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE sonarqube to sonar;"
+    sudo systemctl restart  postgresql
 
-[Install]
-WantedBy=multi-user.target
+# Sonarqube installation and setup
+    sudo mkdir /sonarqube/
+    cd /sonarqube/
+    sudo curl -O https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.3.0.34182.zip
+    sudo apt-get install zip -y
+    sudo unzip -o sonarqube-8.3.0.34182.zip -d /opt/
+    sudo mv /opt/sonarqube-8.3.0.34182/ /opt/sonarqube
+    sudo groupadd sonar
+    sudo useradd -c "SonarQube - User" -d /opt/sonarqube/ -g sonar sonar
+
+    sudo cp /opt/sonarqube/conf/sonar.properties /root/sonar.properties_backup
+    sudo chown sonar:sonar /opt/sonarqube/ -R
+    sudo sh -c 'cat <<EOF> /opt/sonarqube/conf/sonar.properties
+    sonar.jdbc.username=sonar
+    sonar.jdbc.password=admin123
+    sonar.jdbc.url=jdbc:postgresql://localhost/sonarqube
+    sonar.web.host=0.0.0.0
+    sonar.web.port=9000
+    sonar.web.javaAdditionalOpts=-server
+    sonar.search.javaOpts=-Xmx512m -Xms512m -XX:+HeapDumpOnOutOfMemoryError
+    sonar.log.level=INFO
+    sonar.path.logs=logs
 EOF'
+# Setup Systemd service for Sonarqube
+    sudo sh -c 'cat <<EOF> /etc/systemd/system/sonarqube.service
+    [Unit]
+    Description=SonarQube service
+    After=syslog.target network.target
 
-# Start and enable the SonarQube service
-sudo systemctl start sonarqube
-sudo systemctl enable sonarqube
+    [Service]
+    Type=forking
 
-# Check SonarQube service status
-sudo systemctl status sonarqube
+    ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
+    ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
 
-# Open the firewall port 9000 if UFW is enabled
-sudo ufw allow 9000/tcp
-sudo ufw enable
+    User=sonar
+    Group=sonar
+    Restart=always
+
+    LimitNOFILE=65536
+    LimitNPROC=4096
+
+    [Install]
+    WantedBy=multi-user.target
+EOF'
+# Enable and restart service
+    sudo systemctl daemon-reload
+    sudo systemctl enable sonarqube.service
+    sudo systemctl start sonarqube.service
+    sudo /opt/sonarqube/bin/linux-x86-64/sonar.sh start
+    sudo reboot
